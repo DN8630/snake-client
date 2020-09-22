100b7f5c714d2b15508d28f6011d90986697c913
Daya <dayanayak30@gmail.com>
2020-09-22 13:57:09 +0000

    Move Commands

--- client.js
+++ client.js
@@ -13,8 +13,26 @@ const connect = function() {
   conn.setEncoding('utf-8');
   conn.on('connect', () => {
     console.log(`Successfully connected to the game server`);
-    conn.write(`Name: DGN`);
+    conn.write("Name: DAY");
+    conn.write("Move: up");
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // },50);
+    // setTimeout(() => {
+    //   conn.write("Move: left");
+    // },300);
+    // setTimeout(() => {
+    //   conn.write("Move: down");
+    // },500);  setTimeout(() => {
+    //   conn.write("Move: right");
+    // },700);
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // },50);
+ 
+    
   });
+
   conn.on('data',(data) => {
     console.log(`${data}`);
   });
